2b87a947dd78a2b668d312c3493d003026a60ea0
John Sun <john.sun@sjsu.edu>
2016-05-31 15:45:12 +0000

    Added the ability for users to delete stocks from their portfolio.

--- app/assets/javascripts/stocks.js
+++ app/assets/javascripts/stocks.js
@@ -2,22 +2,22 @@ var init_stock_lookup;
 
 init_stock_lookup = function() {
     $('#stock-lookup-form').on('ajax:before', function(event, data, status) {
-        show_spinner(); //Show spinner while the site looks up user's ticker symbol.
+        show_spinner();
     });
     
     $('#stock-lookup-form').on('ajax:after', function(event, data, status) {
-        hide_spinner(); //Hide spinner after the symbol has been found.
+        hide_spinner(); 
     });
     
     $('#stock-lookup-form').on('ajax:success', function(event, data, status) {
-        $('#stock-lookup').replaceWith(data); //Display stock data in the stock-lookup div.
+        $('#stock-lookup').replaceWith(data); 
         init_stock_lookup();
     });
 
     $('#stock-lookup-form').on('ajax:error', function(event, xhr, status) {
-        hide_spinner(); //Hide spinner in case of an invalid stock symbol.
+        hide_spinner();
         $('#stock-lookup-results').replaceWith('  ');
-        $('#stock-lookup-errors').replaceWith('Stock was not found.'); //Display error to user.
+        $('#stock-lookup-errors').replaceWith('Stock was not found.');
     });
 }
 
