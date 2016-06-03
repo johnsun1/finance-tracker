460874e796cbc53e18cb188b01a7c966760e3d89
John Sun <john.sun@sjsu.edu>
2016-06-03 15:42:38 +0000

    Added front end features to allow user to update account information. Also added new links to the navigation bar.

--- app/controllers/user_stocks_controller.rb
+++ app/controllers/user_stocks_controller.rb
@@ -25,7 +25,7 @@ class UserStocksController < ApplicationController
   # POST /user_stocks.json
   def create
     if params[:stock_id].present?
-      @user_stock = UserStock.new(stock_id: params[:stock_id], user: :current_user)
+      @user_stock = UserStock.new(stock_id: params[:stock_id], user: current_user)
     else
       stock = Stock.find_by_ticker(params[:stock_ticker])
       if stock
